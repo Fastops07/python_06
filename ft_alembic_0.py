7090cac1090c0ada15b878f7bf9794fce6ccf2a9
Matthias Voiturier <matthias.voiturier@hotmail.com>
2026-07-15 13:00:05 +0200

    corr de l ex1

--- ft_alembic_0.py
+++ ft_alembic_0.py
@@ -1,13 +1,10 @@
-from elements import create_water
+import elements
 
 
 def main() -> None:
-    print("=== Alembic 1 ===")
-    print(
-        "Using: 'from ... import ...' structure "
-        "to access elements.py"
-    )
-    print(f"Testing create_water: {create_water()}")
+    print("=== Alembic 0 ===")
+    print("Using: 'import ...' structure to access elements.py")
+    print(f"Testing create_fire: {elements.create_fire()}")
 
 
 if __name__ == "__main__":
